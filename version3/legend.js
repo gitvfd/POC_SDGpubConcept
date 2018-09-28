4d2fffc19d7ce833acdd137068d159fee7bf6f10
Vincent <vinz2007@hotmail.com>
2018-09-28 14:51:58 +0200

    new version

---
+++ version3/legend.js
@@ -0,0 +1,79 @@
+
+function drawLegend(){
+  var svglegend = d3.select("#legend").append("svg")
+            .attr("width", width)
+            .attr("height",heightLeg);
+
+  var dataLegend=[{name:"SDG"},{name:"Concept"},{name:"Publication"}]
+
+  var nodeLegend = svglegend
+    .selectAll("g")
+    .data(dataLegend)
+    .enter()
+    .append("g")
+    .attr("class", function(d){
+      return d.name;
+    })
+    .attr("transform",function(d,i){
+          i++;
+          return "translate("+i*width/4 + ","+heightLeg/3 + ")"
+        });
+
+    var radiusLegend=heightLeg/4
+    nodeLegend.append("circle")
+      .attr("r", radiusLegend)
+      .attr("fill", function(d) { 
+        if(d.name=="SDG Concept")
+          return "#123123";
+        else if(d.name=="Pub Concept")
+          return color("Concept");
+        else
+          return color(d.name); })
+
+    .attr("stroke","#939393")
+    .attr("stroke-width",function(d){
+      
+      if(d.name=="SDG")
+        return "2px"
+      else 
+        return "0px";
+    });
+
+        
+    nodeLegend.append("text")
+      .attr("class","usage")
+      .attr("dy", "0.35em")
+      .attr('text-anchor', 'middle')
+      .attr('font-size',"14px")
+      .style("fill","#464646")
+      .style("font-weight","bold")
+      .attr("x",0)
+      .attr("y",radiusLegend+10)
+      .text(function(d){
+        return d.name;
+      })
+      .call(wrap,wrapWidth);
+    
+    /**nodeLegend.append("image")
+        .attr("xlink:href", function(d){
+          var key=key=d.name.replace(/[ .,\/#!$%\^&\*;:{}=\-_`~()]/g,"_"); //see how to remove all special characters no bugs / [.,\/#!$%\^&\*;:{}=\-_`~()]/g
+          var url="icons/"+key+".svg";
+            return url;
+        })
+        .attr("x",  function(d){
+          
+            return -radiusLegend/2;
+        })
+        .attr("y",function(d){
+          
+            return -radiusLegend/2;
+        })
+        .attr("width", function(d){
+          
+            return radiusLegend;
+        })
+        .attr("height",  function(d){
+         
+            return radiusLegend;
+        });**/
+}
