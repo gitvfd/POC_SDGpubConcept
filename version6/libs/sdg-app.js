9bccf9c4950d5f498f21e6b61d32f5eee21d1d6e
Bertrand Riviere <bertrand.riviere@gmail.com>
2019-05-15 15:52:06 +0200

    add sdgAppGetBookmarkedDois that accepts a callbak that will be called with the bookmarked doi list as param the callbak will be called with [] when not embedded in iframe

--- version6/libs/sdg-app.js
+++ version6/libs/sdg-app.js
@@ -10,6 +10,8 @@ const CLOUD_VIZ_REQUEST_DATA = 'CLOUD_VIZ_REQUEST_DATA';
 const SDG_APP_RECEIVE_DATA = 'SDG_APP_RECEIVE_DATA';
 const CLOUD_VIZ_DISPLAY_PUBLICATION = 'CLOUD_VIZ_DISPLAY_PUBLICATION';
 const CLOUD_VIZ_BOOKMARK_PUBLICATION = 'CLOUD_VIZ_BOOKMARK_PUBLICATION';
+const CLOUD_VIZ_REQUEST_BOOKMARKED_DOIS = 'CLOUD_VIZ_REQUEST_BOOKMARKED_DOIS';
+const SDG_APP_RECEIVE_BOOKMARKED_DOIS = 'SDG_APP_RECEIVE_BOOKMARKED_DOIS';
 
 function sdgAppGetData(indicDataUrl, onReceiveDataCallback) {
   const handleReceiveData = function handleReceiveData(e) {
@@ -18,8 +20,8 @@ function sdgAppGetData(indicDataUrl, onReceiveDataCallback) {
         if (error) throw error;
         onReceiveDataCallback(null, e.data.concept, e.data.data, indicData);
       });
+      window.removeEventListener('message', handleReceiveData);
     }
-    window.removeEventListener('message', handleReceiveData);
   };
   window.addEventListener('message', handleReceiveData);
   window.parent.postMessage({ type: CLOUD_VIZ_REQUEST_DATA }, '*');
@@ -34,5 +36,20 @@ function sdgAppDisplayPublication(doi) {
 function sdgAppBookmarkPublication(doi) {
   if (isEmbeddedInIFrame) {
     window.parent.postMessage({ type: CLOUD_VIZ_BOOKMARK_PUBLICATION, doi: doi }, '*');
+  }
+}
+
+function sdgAppGetBookmarkedDois(onReceiveBookmarkedDoisCallback) {
+  if (isEmbeddedInIFrame) {
+    const handleReceiveBookmarkedDois = function handleReceiveBookmarkedDois(e) {
+      if (e.data.type === SDG_APP_RECEIVE_BOOKMARKED_DOIS) {
+        onReceiveBookmarkedDoisCallback(e.data.dois);
+        window.removeEventListener('message', handleReceiveBookmarkedDois);
+      }
+    };
+    window.addEventListener('message', handleReceiveBookmarkedDois);
+    window.parent.postMessage({ type: CLOUD_VIZ_REQUEST_BOOKMARKED_DOIS }, '*');
+  } else {
+    onReceiveBookmarkedDoisCallback([]);
   }
 }
