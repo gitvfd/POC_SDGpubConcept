633e873c23c1238795368e7c1e05af4ffaa6cb0b
Vincent <vinz2007@hotmail.com>
2019-09-25 14:05:22 +0200

    canvas hide

--- version8/libs/sdgSelection.js
+++ version8/libs/sdgSelection.js
@@ -3,6 +3,8 @@ function embeddedView(sdgSelection){
     sdgSelection='no-poverty'
     console.log(sdgSelection)
 
+    d3.select("#canvas").style("opacity", 0.2)
+    d3.select("#canvas-edge").style("opacity", 0)
     setSelectionEmbedView(nodes_raw.filter(function (d) { return d.name == sdgSelection; })[0])
     drawSelected() //Draw the selected nodes
 
